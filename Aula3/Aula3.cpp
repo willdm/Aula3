95cb6ebc82ef536be280236a748609eba3e33955
Willian Miranda <willian.miranda@edu.univali.br>
2018-03-24 18:09:35 -0300

    exc 11

--- Aula3/Aula3.cpp
+++ Aula3/Aula3.cpp
@@ -3,8 +3,9 @@
 
 #include "stdafx.h"
 #include <Windows.h>	
-#include <iostream>  //entrada e saida a a
+#include <iostream>  //entrada e saida 
 #include <string>	//cadeia
+#include "time.h"	//util.sorteia
 
 
 using namespace std;
@@ -36,7 +37,7 @@ int main(){
 	//int n,a=0,b=1,c=1;
 	//cin >> n;
 	//for (c = 1; c <= n; c++) {
-	//	c = a + b;
+	//	c = a + b;3
 	//	a = b;
 	//	b = c;
 	//	cout << c << endl;
@@ -87,29 +88,135 @@ int main(){
 
 #pragma region Exc 6
 	//Faça um programa que solicite um valor N ao usuario. Demontre os numeros primos que aparecem até N.
-	int numeroInicio, numeroFinal, a, b, c, d;
-		cin >> numeroFinal;
+	//int inicio, num_final;
+	//bool ehprimo;
+	//cin >> num_final;
 
-		for (numeroInicio = 2; numeroInicio <numeroFinal; numeroInicio++) {
-			if (numeroInicio == 2 | numeroInicio == 3 | numeroInicio == 5 | numeroInicio == 7) {
-				cout << numeroInicio << " é primo" << endl;
-			}
-			if (numeroInicio % 2 != 0 && numeroInicio % 3 != 0 && numeroInicio % 5 != 0 && numeroInicio % 7 != 0) {
-				cout << numeroInicio << " é primo" << endl;
-			}
-		}
-
+	//for (inicio = 2; inicio <= num_final; inicio++) {
+	//	ehprimo = true;
+	//	for (int i = 2; i < inicio; i++) {
+	//		if (inicio % i == 0) {
+	//			ehprimo = false;
+	//		}
+	//	}
+	//	if (ehprimo) {
+	//		cout << inicio << endl;
+	//	}
+	//}
 #pragma endregion
 
 #pragma region Exc 7
+	//Solicite ao usuário 5 números inteiros e armazene esses números em um array.Exiba o array preenchido;
+		//int c, vetor[5];
+		//for (c = 0; c < 5; c++) {
+		//	cin >> vetor[c];
+		//	}
+		//for (c = 0; c < 5; c++)
+		//{
+		//	cout << vetor[c] << ", ";
+		//}
 
 #pragma endregion 
 
+#pragma region Exc 8
+ /*solicita ao usuário 10 valores reais e armazene em um array.
+ calcule e exiba a média aritmética dos valores.
+ em seguida, exiba todos os valores digitados que estão acima da média calculada.
+	cout << "escreva dez notas de provas: " << endl;	
+	double media= 0.0 , vetor[10];
+	for (int c = 0; c < 10; c++){
+		cin >> vetor[c];
+		media += vetor[c];
+		}
+	media = media / 10;
+	cout << "notas acima da média: " << "(" << media << ")" << endl;
+	for (int c = 0; c < 10; c++){
+		if (vetor[c] >= media) {
+			cout << vetor[c] << "	";
+		}
+	}*/
+#pragma endregion
 
+#pragma region Exc 9
+	/*Faça um algoritmo em que o usuário preencha dois arrays de 10 posições.
+	O programa deve fazer a multiplicação dos elementos de mesmo índice, colocando o resultado em um terceiro array.Exiba o array resultante.*/
+	/*cout << "escreva 2 vezes dez numeros de provas: " << endl;
+	int media = 0.0, vet1[10], vet2[10], vetr[10] ;
+	for (int c = 0; c < 10; c++) {
+		cin >> vet1[c];
+	}
+	for (int c = 0; c < 10; c++) {
+		cin >> vet2[c];
+	}
+	for (int c = 0; c < 10; c++) {
+		vetr[c] = vet1[c] * vet2[c];
+		cout << vetr[c] << "	";
+	}
+	cout << endl;*/
+#pragma endregion
 
+#pragma region Exc 10
+	//Preencha um array de 50 posições com números aleatórios entre 1 e 50 e exiba :
+	//O menor valor contido no array;
+	//O índice do menor valor contido no array;
+	//A quantidade de números ímpares contidos no array;
+	//Todos os números pares contidos no array.
 
+	srand(time(NULL));
+	int mv, imv, qtdimp = 0, qtdpares = 0, vet[50];
+	int op;
+		for (int c = 0; c < 50; c++) {
+			vet[c] = rand() % 100;
+			cout << vet[c] << "	";
+		}
 
+		for (int i = 0; i < 50; i++) {
+			if (i == 0) {
+				mv = vet[i];
+				imv = i;
+			}
+			if (vet[i] < mv) {
+				mv = vet[i];
+				imv = i;
+			}
+			if (vet[i] % 2 == 0) {
+				qtdpares += 1;
+			}
+			else {
+				qtdimp += 1;
+			}
+		}
 
+		cin >> op;
+		switch (op){
+		case 1:
+			cout << "O menor valor contido no array é : " << mv << endl;
+			break;
+		case 2:
+			cout << "O índice do menor valor contido no array é : " << imv << endl;
+			break;
+		case 3:
+			cout << "A quantidade de números ímpares contidos no array é : " << qtdimp << endl;
+			break;
+		case 4:
+			cout << "Todos os números pares contidos no array é : " << qtdpares << endl;
+			break;
+		default:
+			break;
+		}
+#pragma endregion
+
+#pragma region Exc 11
+	//Faça um algoritmo que preencha uma matriz 3x3 de números inteiros com um valor digitado pelo usuário em todas as posições e exiba a matriz
+	/*int colun, line, n = 3;
+	cout << "Preencha uma matriz de 3X3";
+
+	for (colun = 0; colun < n; colun++) {
+		for (line = 0; line < n; line++) {
+			cin >> line[];
+		}
+	}*/
+#pragma endregion
 
     return 0;
 }
